66e72a32d054b4fa68bd08793b6a53ccf6ae3c67
Sharuma <gr33nmous3@gmail.com>
2014-04-20 16:14:09 +0530

    it works as intended now

---
+++ README.md
@@ -0,0 +1,7 @@
+# Conditional Turn Based System
+
+characters[n:not(current)].time -= characters[current].time;
+
+## players.json
+
+```"name": [HP, Damage, Attack wait time, Heal wait time]```
--- cbt.js
+++ cbt.js
@@ -1,71 +1,111 @@
-// Conditional Turn Based System
-// It's sloppy, and doesn't allow people of exactly the same agility, but it works.
-// Players = "Name": [Agility, Damage, currentHP, maxHP]
+//Conditional Turn Based System
+var _players = require("./players.json");
 
-var players = require("./players.json"), rules = require("./rules.json"), waittime = {}, waittime_d = {};
-var order = {}, alivePlayers = 0;
+var Skill = function(player, name, modifier, fn) {
+	this.player = player;
+	this.name = name;
+	this.modifier = modifier;
+	this.skillfn = fn;
+}
 
-Object.keys(players).forEach(function(player) {
-	console.log(player, "enters the arena");
-	waittime[player] = rules.maxWT - players[player][0];
-	waittime_d[player] = rules.maxWT - players[player][0];
-	order[waittime[player]] = player;
-	alivePlayers += 1;
-});
-
-while(alivePlayers > 1) {
-	var Order = ["Attack", "Attack", "Attack", "Item"];
-	var PlayerName = order[Object.keys(order)[0]], Player = players[PlayerName], wt = waittime[order[Object.keys(order)[0]]];
-
-	if(Player[2] < Player[3]/4) Order = ["Item", "Item", "Item", "Attack"];
-	if(Player[2] >= Player[3]) {
-		Order = ["Attack", "Attack", "Attack", "Attack"];
-		Player[2] = Player[3];
+Skill.prototype = {
+	getWaitTime: function() {
+		return this.player[this.modifier];
+	},
+	getEffects: function(target) {
+		return this.skillfn(this.player, target);
 	}
+}
 
-	var Action = Order[Math.floor(Math.random() * 4)], Action2 = "";
+var Player = function(data) {
+	this.hp = data[0];
+	this.mhp = data[0];
+	this.atk = data[1];
+	this.aspd = data[2];
+	this.mspd = data[3];
+	this.man = 200;
 
-
-	Object.keys(waittime).forEach(function(wait) {
-		if(players[wait][2] < 1) return;
-		waittime[wait] -= rules.WTAuto;
-		if(waittime[wait] < 1) waittime[wait] = 0;
-	})
-
-	waittime[PlayerName] = waittime_d[PlayerName] * rules[Action];
-
-	if(Action == "Attack") {
-		Action2 = PlayerName;
-		wt = wt * rules[Action];
-		while(Action2 == PlayerName) {
-			Action2 = Object.keys(players)[Math.floor(Math.random() * Object.keys(players).length)];
-			if(players[Action2][2] < 1) Action2 = PlayerName;
-		}
-		Action = "attacks the";
-		var DMG = Player[1];
-		if(Math.random() > 0.5) DMG *= (Math.random() * 2);
-		else DMG *= Math.random();
-		DMG = Math.ceil(DMG);
-		players[Action2][2] -= DMG;
-		console.log(PlayerName, Action, Action2, "("+DMG+")");
-	} else {
-		Action = "heals itself.";
-		Player[2] += rules.ItemHeal;
-		console.log(PlayerName, Action);
-	}
-
-
-	if(Action2 != "") {
-		if(players[Action2][2] < 1) {
-			console.log(PlayerName, "as slain", Action2+"!");
-			alivePlayers--;
-		}
-	}
-
-	order = {};
-	Object.keys(waittime).forEach(function(wait) {
-		if(players[wait][2] < 1) return;
-		order[waittime[wait]] = wait;
+	var self = this;
+	this.skills = {}
+	this.skills.heal = new Skill(self, "heals", "mspd", function(player, target) {
+		var hp = Math.round(player.mhp * 0.20);
+		player.hp = player.hp + hp;
+		return [0, hp];
+	});
+	this.skills.attack = new Skill(self, "attacks", "aspd", function(player, target) {
+		var dam = player.atk + Math.round(Math.random() * (player.atk * 0.2) - (player.atk * 0.1));
+		target.hp = target.hp - dam;
+		return [dam, 0];
 	});
 }
 
+Player.prototype = {
+	//super basic AI
+	getSkill: function() {
+		if(this.hp / this.mhp < 0.70 && this.man > 0) {
+			return this.skills.heal;
+		}
+		return this.skills.attack;
+	},
+	isDead: function() {
+		return this.hp < 1;
+	}
+}
+
+var players = {}, order = [];
+
+for(var name in _players) {
+	players[name] = new Player(_players[name]);
+	order.push([name, players[name].getSkill().getWaitTime()]);
+}
+
+order = order.sort(function(a, b) {
+	return a[1] - b[1];
+});
+
+
+var aliveCount = function(players) {
+	var i = 0;
+	for(var name in players) {
+		if(!players[name].isDead()) ++i;
+	}
+	return i;
+}
+
+var getOpponents = function(players, me) {
+	var opponents = [];
+	for(var name in players) {
+		if(players[name].isDead()) continue;
+		if(name != me) opponents.push(name);
+	}
+	return opponents;
+}
+
+while(aliveCount(players) > 1) {
+	var current = order.shift();
+	if(players[current[0]].isDead()) continue;
+	for(var i = 0; i < order.length; ++i) {
+		order[i][1] -= current[1];
+	}
+
+	var opponents = getOpponents(players, current[0]);
+	var opponent_name = opponents[Math.floor(Math.random() * opponents.length)];
+	var opponent = players[opponent_name];
+	var skill = players[current[0]].getSkill();
+	var effects = skill.getEffects(opponent);
+
+	if(effects[0] > 0) {
+		console.log(current[0], skill.name, opponent_name, "attacks for", effects[0], "damage");
+	}
+	if(effects[1] > 0) {
+		console.log(current[0], skill.name, "for", effects[1], "health");
+	}
+	if(opponent.isDead()) {
+		console.log(current[0], "has slain", opponent_name);
+	}
+
+	order.push([current[0], players[current[0]].getSkill().getWaitTime()]);
+	order = order.sort(function(a, b) {
+		return a[1] - b[1];
+	});
+}
